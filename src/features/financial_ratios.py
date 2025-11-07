d85b2e1947ec36e9b4bc972f617087aed2845bbe
rp0201 <rithi567@icloud.com>
2025-11-07 13:01:05 -0500

    feat: added bankruptcy model + financial ratios

--- src/features/financial_ratios.py
+++ src/features/financial_ratios.py
@@ -7,7 +7,6 @@ def get_liquidity_ratios(facts: dict) -> dict:
 def get_leverage_ratios(facts: dict) -> dict:
     return {
         'debt_to_equity': facts['total_liabilities'] / facts['stockholders_equity'],
-        # Need to parse interest expense
         'interest_coverage_ratio': (facts['revenue'] - facts['cost_of_goods_sold'] - facts['operating_expenses']) / facts['interest_expense']
     }
 
@@ -20,33 +19,5 @@ def get_profitability_ratios(facts: dict) -> dict:
 def get_cash_flow_ratios(facts: dict) -> dict:
     return {
         'operating_cash_flow': facts['operating_cash_flow'] / facts['current_liabilities'],
-        # need to parse capital expenditures
         'free_cash_flow_to_assets': (facts['operating_cash_flow'] - facts['capital_expenditure']) / facts['total_assets']
     }
-
-'''
-        ===== All Metrics for Ratios =====
-        tags = {
-        'total_assets': 'Assets',
-        'current_assets': 'AssetsCurrent',
-        'cash': 'CashAndCashEquivalentsAtCarryingValue',
-        'accounts_receivable': 'AccountsReceivableNetCurrent',
-        'inventory': 'InventoryNet',
-        'total_liabilities': 'Liabilities',
-        'current_liabilities': 'LiabilitiesCurrent',
-        'stockholders_equity': 'StockholdersEquity',
-        'retained_earnings': 'RetainedEarningsAccumulatedDeficit',
-        'revenue': 'Revenues',
-        'cost_of_revenue': 'CostOfRevenue', 
-        'gross_profit': 'GrossProfit',
-        'operating_income': 'OperatingIncomeLoss',
-        'net_income': 'NetIncomeLoss',
-        'earnings_per_share': 'EarningsPerShareBasic',
-        'cost_of_goods_sold': 'CostOfGoodsAndServicesSold',
-        'operating_expenses': 'OperatingExpenses'
-        'financing_cash_flow': 'NetCashProvidedByUsedInFinancingActivities',
-        'investing_cash_flow': 'NetCashProvidedByUsedInInvestingActivities', 
-        'operating_cash_flow': 'NetCashProvidedByUsedInOperatingActivities',
-        'depreciation': 'DepreciationDepletionAndAmortization'
-        }
-'''
--- src/models/bankruptcy_score.py
+++ src/models/bankruptcy_score.py
@@ -60,36 +60,8 @@ def get_ohlson_oscore(facts: dict) -> float:
     ffo = facts['net_income_current'] + facts['depreciation']
     intwo = 1 if nic < 0 and nil < 0 else 0
     oeneg = 1 if tl > ta else 0
-    chin = 0 if abs(nic) + abs(nil) == 0 else chin = (nic - nil) / (abs(nic) + abs(nil))
+    chin = 0 if (abs(nic) + abs(nil) == 0) else ((nic - nil) / (abs(nic) + abs(nil)))
 
     o_score = -1.32 - 0.407*math.log(ta / 1000) + 6.03*(tl/ta) - 1.43*(wc/ta) + 0.0757*(cl/ca) - 2.37*(nic/ta) - 1.83*(ffo/tl) + 0.285*intwo - 1.72*oeneg - 0.521*chin
 
     return o_score
-
-
-if __name__ == '__main__':
-    facts = {
-        'total_assets': 122070000000,
-        'current_assets': 58360000000,
-        'cash': 16139000000,
-        'accounts_receivable': 4418000000,
-        'inventory': 12017000000,
-        'total_liabilities': 48390000000,
-        'current_liabilities': 28821000000,
-        'stockholders_equity': 72913000000,
-        'retained_earnings': 35209000000,
-        'revenue': 97690000000,
-        'cost_of_revenue': 80240000000,
-        'gross_profit': 17450000000,
-        'operating_income': 7076000000,
-        'earnings_per_share': 2.23,
-        'financing_cash_flow': 3853000000,
-        'investing_cash_flow': -18787000000,
-        'operating_cash_flow': 14923000000,
-        'net_income_current': 7091000000,
-        'net_income_last': 14997000000,
-        'depreciation': 769300000
-    }
-
-    print(get_ohlson_oscore(facts))
-    print(get_altman_zscore(facts))
